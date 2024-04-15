735027ac3ce5c31a0f93dfc6875df69930dd6e12
sunitha arockia dass <sunithatheresa18@gmail.com>
2024-04-15 23:01:10 +0200

    main.jsx changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router >
+    <Router basename="/portfolio">
       <App />
     </Router>
   </React.StrictMode>
