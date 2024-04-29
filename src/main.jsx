66d93000b8d8633fdf063c14fa44ca35a3ececbc
sunitha arockia dass <sunithatheresa18@gmail.com>
2024-04-30 00:11:55 +0200

    deploying in vercel

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router 
-    basename="/sunitha-portfolio"
+    // basename="/sunitha-portfolio"
     >
       <App />
     </Router>
